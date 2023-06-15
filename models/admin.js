2cf1aadb0e69f6292a600c3faee1b3cac65d3d59
Itan Moshe <itanmoshe@gmail.com>
2023-06-15 23:32:27 +0300

    server init

---
+++ models/admin.js
@@ -0,0 +1,32 @@
+const mysql = require("./mySqlPool")
+
+const insertAdmin = (name,email,phoneNumber,hashedPassword)=>{
+    return mysql.execute(`
+    INSERT INTO the_book_project.admin (name, email, phone_number, hashed_password) VALUES (?, ?, ?, ?)
+`,[name,email,phoneNumber,hashedPassword])
+}
+
+const getAdmin = (email)=>{
+    return mysql.execute(`SELECT * FROM admin WHERE email = ?`,[email])
+}
+
+const chaeckIfAdminExist = ()=>{
+    return mysql.execute(`SELECT * FROM admin`)
+}
+
+const updatePassword = (password,email)=>{
+    return mysql.execute(`UPDATE admin SET hashed_password = ? WHERE (email = ?);
+`,[password,email])
+}
+
+const updateAdminDetails = (name,email,phonenumber)=>{
+    return mysql.execute(`UPDATE admin SET name = ? , email = ? , phone_umber = ? , WHERE email = ?`,[name,email,phonenumber,email])
+}
+
+module.exports = {
+    insertAdmin,
+    getAdmin,
+    updatePassword,
+    chaeckIfAdminExist,
+    updateAdminDetails
+}
---
+++ routes/api.js
@@ -0,0 +1,12 @@
+const router = require('express').Router()
+
+router.get("/",(req,res)=>{
+    try{
+
+    }
+    catch(err){
+        
+    }
+})
+
+module.exports = router
